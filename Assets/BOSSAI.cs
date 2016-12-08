6d557dde746976054b69c71e54b32682d2b91f81
PCFreeman <freemanfrank96@gmail.com>
2016-12-07 21:02:59 -0800

    new one

--- Assets/BOSSAI.cs
+++ Assets/BOSSAI.cs
@@ -8,7 +8,12 @@ public class BOSSAI : MonoBehaviour
     public int moveSpeed;
     private Animator myAnimator;
     private Vector2 previousLocation;
-   
+    public int MaxDist = 10;
+    public int MinDist = 10;
+    public float laserspeed = 3f;
+    public GameObject BossFirepoint;
+    public GameObject BossLaser;
+
     void Awake()
     {
         myAnimator = GetComponent<Animator>();
@@ -29,26 +34,56 @@ public class BOSSAI : MonoBehaviour
         previousLocation = transform.position;
 
         //Move Towards Target
-        transform.position += (target.position - transform.position).normalized * moveSpeed * Time.deltaTime;
+        if (Vector3.Distance(transform.position, target.position) <= MinDist)
+        {
 
-        if(previousLocation.x - transform.position.x>=0)
+            transform.position += (target.position - transform.position).normalized * moveSpeed * Time.deltaTime;
+            if (previousLocation.x - transform.position.x >= 0)
         {
             transform.localScale = new Vector3(-1, 1, 1);
             myAnimator.SetBool("IsWalk", true);
         }
-        else if(previousLocation.x - transform.position.x <= 0)
+        else if (previousLocation.x - transform.position.x <= 0)
         {
-              transform.localScale = new Vector3(1, 1, 1);
+            transform.localScale = new Vector3(1, 1, 1);
             myAnimator.SetBool("IsWalk", true);
         }
-        if ((int) previousLocation.y != (int) transform.position.y)
+        if ((int)previousLocation.y != (int)transform.position.y)
         {
             myAnimator.SetBool("IsJump", true);
         }
-        else
+            GameObject bullet = Instantiate(BossLaser, BossFirepoint.transform.position, transform.rotation) as GameObject;
+            bullet.GetComponent<Rigidbody2D>().velocity = Vector3.right * laserspeed;
+            if (target.position.y-transform.position.y<0)
+            {
+                bullet.transform.eulerAngles = new Vector3(0, 0, -90);
+                bullet.GetComponent<Rigidbody2D>().velocity = Vector3.down * laserspeed;
+            }
+            if (Input.GetKey(KeyCode.UpArrow) && Input.GetKeyDown(KeyCode.Z))
+            {
+                bullet.transform.eulerAngles = new Vector3(0, 0, 90);
+                bullet.GetComponent<Rigidbody2D>().velocity = Vector3.up * laserspeed;
+            }
+       if (transform.localScale.x < 0)
         {
-            myAnimator.SetBool("IsJump", false);
+            bullet.transform.localScale = new Vector3(-1, 1, 1);
+            bullet.GetComponent<Rigidbody2D>().velocity = Vector3.left * laserspeed;
+            if (Input.GetKey(KeyCode.DownArrow) && Input.GetKeyDown(KeyCode.Z))
+            {
+                bullet.transform.eulerAngles = new Vector3(0, 0, -90);
+                bullet.GetComponent<Rigidbody2D>().velocity = Vector3.down * laserspeed;
+            }
+            if (Input.GetKey(KeyCode.UpArrow) && Input.GetKeyDown(KeyCode.Z))
+            {
+                bullet.transform.eulerAngles = new Vector3(0, 0, 90);
+                bullet.GetComponent<Rigidbody2D>().velocity = Vector3.up * laserspeed;
+            }
         }
+        else
+        { myAnimator.SetBool("IsWalk", false); }
+
+        }
+      
     }
-    round
+   
 }
